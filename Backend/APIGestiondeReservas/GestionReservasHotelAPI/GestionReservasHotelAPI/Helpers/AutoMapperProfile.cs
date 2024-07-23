26cbe5697d640d84077e3c897ce6834b41a36460
CristianGmz7 <cristiangomez.cagg78@gmail.com>
2024-07-22 23:22:17 -0600

    Agregar archivo AutoMapperProfile.cs

---
+++ Backend/APIGestiondeReservas/GestionReservasHotelAPI/GestionReservasHotelAPI/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using GestionReservasHotelAPI.Dtos.Reservations;
+
+namespace GestionReservasHotelAPI.Helpers;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+    }
+}

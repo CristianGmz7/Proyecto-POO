31afb024cce990ecc29d63c22a86744ac22bc3ab
CristianGmz7 <cristiangomez.cagg78@gmail.com>
2024-08-16 05:46:01 -0600

    maquetando_componente_navegacion

--- Frontend/gestion-reservas-hotel-frontend/gestion-reservas-hotel-frontend/src/App.jsx
+++ Frontend/gestion-reservas-hotel-frontend/gestion-reservas-hotel-frontend/src/App.jsx
@@ -1,14 +1,16 @@
 // import { BrowserRouter } from "react-router-dom";
 // import { AppRouter } from "./routes/AppRouter";
 import {RoomList} from "./features/client/pages"
+import {Nav} from "./features/client/components"
 
 export const App = () => {
   return (
     // <BrowserRouter>
     //   <AppRouter />
     // </BrowserRouter>
-    // <div className="bg-yellow-500 w-full h-full text-4xl">Hola</div>
-    // <RoomList/>
-    <RoomList />
+    <>
+      <Nav />
+      <RoomList />
+    </>
   );
 };

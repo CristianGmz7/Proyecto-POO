8c770ca80a70de0dd56bde8fa858e96f71749171
anyta58 <henriquezvictoria58@gmail.com>
2024-08-16 12:23:08 -0600

    Homepage y footer

---
+++ Frontend/gestion-reservas-hotel-frontend/gestion-reservas-hotel-frontend/src/features/client/components/Footer.jsx
@@ -0,0 +1,20 @@
+export const Footer = () => {
+  return (
+    <footer className="px-6 py-10">
+    <div className="container flex flex-col items-center justify-between mx-auto md:flex-row">
+      <a
+        className="text-xl font-bold text"
+        href=""
+      >
+        Reservas-Hotel
+      </a>
+      `
+      <p className="mt-2 text-sm md:mt-0">
+        Todos los derechos reservados 2024
+      </p>
+      `
+    </div>
+  </footer>
+  )
+}
+  
